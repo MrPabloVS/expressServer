4aea15c2ee4b1a37d53bd26ac5607ccf4f705f0f
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-01-21 11:04:14 -0300

    Consignas comentadas

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// 1) Realizar un proyecto en node.js que utilize el middlewere express e implemente los siguientes endpoints en el puerto 8080
+
 const express = require('express')
 const fs = require("fs")
 
@@ -8,25 +10,28 @@ const app = express()
 
 const PORT = 8080
 
-const db = fs.readFileSync("../productos.json")
+const db = fs.readFileSync("productos.json")
 
 const server = app.listen(PORT, ()=> {
 
 })
 
+// a) Ruta get '/productos' que devuelva un array con todos los productos disponibles en el servidor
 app.get('/productos', (request, response) => {
     response.send(db)
 })
 
+// c) Ruta get '/producto/id'
 app.get('/producto/id', (request, response) => {
     response.send(db.producto.id)
 })
 
+// b) Ruta post '/producto'
 app.post('/producto', (request, response) => {
     const body = request.body
-    const {id, title, img, price} = body
+    const { title, img, price} = body
     let hashId = ""
-    bcrypt.hash(id, saltRounds, function(err, hash) {
+    bcrypt.hash(db.length + 1, saltRounds, function(err, hash) {
         hashId = hash
     });
 
